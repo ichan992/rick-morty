f1dc3c0ebacb9692090da16fde69192d633fb772
ichan992 <vidalmarkchristian816@gmail.com>
2026-04-24 18:50:07 +0800

    revert

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
-const repoName = "rick-morty";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: process.env.NODE_ENV === "production" ? `/${repoName}` : "",
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? `/${repoName}/` : "",
-  trailingSlash: true,
+ allowedDevOrigins: ['192.168.1.9'],
 };
 
 export default nextConfig;
